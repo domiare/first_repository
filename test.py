a5d420306a58a32587efe52bc5b7d507e9050e11
Test User <test2410aa@gmail.com>
2021-10-14 19:06:58 +0300

    ok

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
 c = [a + b for a in "welcome" if a != "e" for b in "Python" if b != "t" and b != "h"]
 print(c)
-a = bcd # Just for test
+# Just for test
 b = c # new_branch
+d ="I'm a math"
+s = 'abcdefg'
+print(s[::-3])
